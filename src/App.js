786d08276a0d2e3021394912d05c13b98a7609c1
christol <christol.beresford@hotmail.co.uk>
2023-07-20 16:34:18 +0100

    small updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import Header from "./Components/Header";
 import Main from "./Components/Main";
+import Hornedbeasts from "./Components/Hornedbeasts";
+import data from "./data.json";
+
 function App() {
   const horned1 = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Male_impala_profile.jpg/800px-Male_impala_profile.jpg"
   const horned2 = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Serengeti_Bueffel2.jpg/800px-Serengeti_Bueffel2.jpg"
@@ -10,8 +13,16 @@ function App() {
       <Main title="Big Horn 1" imgUrl={horned1} para="This animal has big horns"></Main>
       <Main title="Big Horn 2" imgUrl={horned2} para="This animal has cool horns"></Main>
       <h1>Hello World</h1>
+   
     </div>
   );
+let MyArr= [1,2,3,4,5,6]
+MyArr.map(function(Hornedbeasts) {
+
+}
+data.map(obj),
+
 }
 
 export default App;
+
